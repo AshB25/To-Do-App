0dbec928667420004b3f881d9e1f8a9913adf346
Ashleigh Carter <ashbcarter@gmail.com>
2024-01-24 00:02:26 -0500

    fixed button how i had it before

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,11 +5,8 @@ import AddTaskForm from '../AddTaskForm/AddTaskForm';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import { createTheme, ThemeProvider  } from '@mui/material/styles';
-import ButtonColor from '../Button/Button';
-import { styled } from '@mui/material/styles';
-import Switch from '@mui/material/Switch';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
+// import ButtonColor from '../Button/Button';
+
 
 
 import './App.css';
@@ -66,56 +63,6 @@ function App () {
     },
   });
 
-  const taskSwitch = styled((props) => (
-    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-  ))(({ theme }) => ({
-    width: 42,
-    height: 26,
-    padding: 0,
-    '& .MuiSwitch-switchBase': {
-      padding: 0,
-      margin: 2,
-      transitionDuration: '300ms',
-      '&.Mui-checked': {
-        transform: 'translateX(16px)',
-        color: '#fff',
-        '& + .MuiSwitch-track': {
-          backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
-          opacity: 1,
-          border: 0,
-        },
-        '&.Mui-disabled + .MuiSwitch-track': {
-          opacity: 0.5,
-        },
-      },
-      '&.Mui-focusVisible .MuiSwitch-thumb': {
-        color: '#33cf4d',
-        border: '6px solid #fff',
-      },
-      '&.Mui-disabled .MuiSwitch-thumb': {
-        color:
-          theme.palette.mode === 'light'
-            ? theme.palette.grey[100]
-            : theme.palette.grey[600],
-      },
-      '&.Mui-disabled + .MuiSwitch-track': {
-        opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
-      },
-    },
-    '& .MuiSwitch-thumb': {
-      boxSizing: 'border-box',
-      width: 22,
-      height: 22,
-    },
-    '& .MuiSwitch-track': {
-      borderRadius: 26 / 2,
-      backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
-      opacity: 1,
-      transition: theme.transitions.create(['background-color'], {
-        duration: 500,
-      }),
-    },
-  }));
 
   return (
     <div className='App'>
@@ -129,8 +76,8 @@ function App () {
           <Grid item xs={5} key={dataIndex}>  
             <h3>{taskData.User}</h3>
             <p>{taskData.Task}</p>
-            <ButtonColor onClick={() => handleClickToggleTask(taskData.id)}> :{taskData.Completed ? 'Yes' : 'No'} </ButtonColor>
-            {/* <Button variant="contained" size="small" color="error" onClick={() => handleClickToggleTask(taskData.id)}>Completed: {taskData.Completed ? 'Yes' : 'No'}</Button> */}
+            {/* <ButtonColor onClick={() => handleClickToggleTask(taskData.id)}> :{taskData.Completed ? 'Yes' : 'No'} </ButtonColor> */}
+            <Button variant="contained" size="small" color="error" onClick={() => handleClickToggleTask(taskData.id)}>Completed: {taskData.Completed ? 'Yes' : 'No'}</Button>
             <Button variant="contained" size="small" color="black" onClick={(event) => handleClickDelete(taskData.id)}>Delete</Button>
           </Grid>
           </ThemeProvider>
